6f69a75c733ff9091e379e916b65cd0606875831
Sergey <fserega2014@yandex.ru>
2023-03-18 12:53:09 +0300

    feat: add components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './header/auth/auth.component';
+import { LogoComponent } from './header/logo/logo.component';
+import { SearchFormComponent } from './header/search-form/search-form.component';
+import { SettingButtonComponent } from './header/setting-button/setting-button.component';
+import { SortFilterComponent } from './header/sort-filter/sort-filter.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchItemsComponent } from './search/search-items/search-items.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    LogoComponent,
+    SearchFormComponent,
+    SettingButtonComponent,
+    SortFilterComponent,
+    SearchItemComponent,
+    SearchItemsComponent,
+    HeaderComponent,
+    SearchComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
