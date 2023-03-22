6e43a30c8b9cae3389cef29faa8a565e9c09d0ea
Sergey <fserega2014@yandex.ru>
2023-03-22 19:11:12 +0300

    refactor: del components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './components/header/auth/auth.component';
-import { LogoComponent } from './components/header/logo/logo.component';
-import { SearchFormComponent } from './components/header/search-form/search-form.component';
-import { SettingButtonComponent } from './components/header/setting-button/setting-button.component';
-import { SortFilterComponent } from './components/header/sort-filter/sort-filter.component';
 import { SearchItemComponent } from './components/search/search-item/search-item.component';
 import { SearchItemsComponent } from './components/search/search-items/search-items.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent,
-    LogoComponent,
-    SearchFormComponent,
-    SettingButtonComponent,
-    SortFilterComponent,
-    SearchItemComponent,
-    SearchItemsComponent,
-    HeaderComponent,
-    SearchComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, SearchItemComponent, SearchItemsComponent, HeaderComponent, SearchComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
